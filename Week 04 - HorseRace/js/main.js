4baf4a2f3a9c24c8dd9006c365d55980bda143e1
Nikola <nikola.kuzmanovski@it-labs.com>
2019-01-30 16:00:48 +0100

    Horse race finished

--- Week 04 - HorseRace/js/main.js
+++ Week 04 - HorseRace/js/main.js
@@ -1,71 +1,106 @@
-var startBtn = document.getElementById('start-btn');
-var restartBtn = document.getElementById('restart-btn');
-var goldRes, whiteRes, brownRes;
+var startBtn = document.getElementById("start-btn");
+var runAnim = document.querySelectorAll(".run-anim");
+var getSign = document.querySelector(".sign");
+var swingSign = document.querySelector(".holder-swing");
+var goldHorse = document.querySelector(".horse-gold");
+var whiteHorse = document.querySelector(".horse-white");
+var brownHorse = document.querySelector(".horse-brown");
+
+var goldRes, whiteRes, brownRes, max;
 
 var randomNo = function getRandom(min, max) {
-    min = Math.ceil(6);
-    max = Math.floor(10);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+  min = Math.ceil(6);
+  max = Math.floor(9);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-function run() {
-
-    var runAnim = document.querySelectorAll('.run-anim');
-    var goldHorse = document.querySelector('.horse-gold');
-    var whiteHorse = document.querySelector('.horse-white');
-    var brownHorse = document.querySelector('.horse-brown');
-    for (var i = 0; i < runAnim.length; i++) {
-        // Start the animation
-        runAnim[i].style.webkitAnimationPlayState = "running";
-    }
-
-    debugger;
-    // Add random time
-    goldRes = goldHorse.style.animationDuration = randomNo() + "s";
-    whiteRes = whiteHorse.style.animationDuration = randomNo() + "s";
-    brownRes = brownHorse.style.animationDuration = randomNo() + "s";
-
-    // Toggle animate class
-    goldHorse.classList.toggle('animate');
-    whiteHorse.classList.toggle('animate');
-    brownHorse.classList.toggle('animate');
-
-}
-
-function toggleText() {
-    var text = startBtn.firstChild;
-    text.data = text.data == 'Start' ? 'Stop' : 'Start';
-}
-
-startBtn.addEventListener('click', () => {
-    run();
-    toggleText();
-    results();
-    end();
+startBtn.addEventListener("click", () => {
+  start();
+  timeout();
 });
 
-function results() {
-    debugger;
-    if (goldRes == whiteRes) {
-        console.log('Gold and White are tie!');
-    } else if (goldRes == brownRes) {
-        console.log('Gold and Brown are tie!');
-    } else if (whiteRes == goldRes) {
-        console.log('White and Gold are tie!');
-    } else if (whiteRes == brownRes) {
-        console.log('White and Brown are tie!');
-    } else if (goldRes < whiteRes || goldRes < brownRes) {
-        console.log("Gold Wins!");
-    } else if (whiteRes < goldRes || whiteRes < brownRes) {
-        console.log('White Wins!');
-    } else if (brownRes < goldRes || brownRes < whiteRes) {
-        console.log('Brown Wins!');
-    }
+function start() {
+  for (var i = 0; i < runAnim.length; i++) {
+    // Start the animation
+    runAnim[i].style.webkitAnimationPlayState = "running";
+  }
+
+  // Add random time
+  goldRes = goldHorse.style.animationDuration = randomNo() + "s";
+  whiteRes = whiteHorse.style.animationDuration = randomNo() + "s";
+  brownRes = brownHorse.style.animationDuration = randomNo() + "s";
+ 
+  // Toggle animate class
+  goldHorse.classList.toggle("animate");
+  whiteHorse.classList.toggle("animate");
+  brownHorse.classList.toggle("animate");
+
+  // Add styles to disable button
+  startBtn.disabled = true;
+  startBtn.style.cursor = "not-allowed";
+  getSign.style.opacity = "0.6";
+  startBtn.style.opacity = "0.6";
+
+  swingSign.style.display = "none";
+}
+
+function timeout() {
+  // Make integer from results and get the largest
+  var goldInteger = parseInt(goldRes, 10);
+  var whiteInteger = parseInt(whiteRes, 10);
+  var brownInteger = parseInt(brownRes, 10);
+  var numbers = [goldInteger, whiteInteger, brownInteger];
+  max = Math.max.apply(null, numbers) * 1000;
+
+  function timer() {
+    setTimeout(function () {
+      end();
+      results();
+    }, max);
+  }
+  timer();
 }
 
 function end() {
-    var goldInteger = parseInt(goldRes, 10);
-    var whiteInteger = parseInt(whiteRes, 10);
-    var brownInteger = parseInt(brownRes, 10);
+  // Reset the button after animation ends
+  function resetBtn() {
+    startBtn.disabled = false;
+    startBtn.style.opacity = "1";
+    startBtn.style.cursor = "pointer";
+    getSign.style.opacity = "1";
+  }
+  resetBtn();
+
+  // Remove animate class after animation ends
+  function removeAnim() {
+    goldHorse.classList.remove("animate");
+    whiteHorse.classList.remove("animate");
+    brownHorse.classList.remove("animate");
+  }
+  removeAnim();
+
+  swingSign.style.animationName = "swing";
+  swingSign.style.display = "block";
 }
 
+// Get the results
+function results() {
+  let winner = document.getElementById('winner');
+  if (goldRes == whiteRes && goldRes < brownRes) {
+    winner.innerHTML = "Gold and White are tie!";
+  } else if (goldRes == brownRes && goldRes < whiteRes) {
+    winner.innerHTML = "Gold and Brown are tie!";
+  } else if (whiteRes == goldRes && whiteRes < brownRes) {
+    winner.innerHTML = "White and Gold are tie!";
+  } else if (whiteRes == brownRes && whiteRes < goldRes) {
+    winner.innerHTML = "White and Brown are tie!";
+  } else if (brownRes == goldRes && brownRes == whiteRes) {
+    winner.innerHTML = "All horses are tie!";
+  } else if (goldRes < whiteRes && goldRes < brownRes) {
+    winner.innerHTML = "Gold horse Wins!";
+  } else if (whiteRes < goldRes && whiteRes < brownRes) {
+    winner.innerHTML = "White horse Wins!";
+  } else if (brownRes < goldRes && brownRes < whiteRes) {
+    winner.innerHTML = "Brown horse Wins!";
+  }
+}
